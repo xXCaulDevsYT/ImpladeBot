33702eedd410c9a1b8f51c94db7127103ef80abf
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-06 19:12:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('on Sleep time! + 8 hours on school with exams!', {type: 'PLAYING'});
+    client.user.setActivity('for school exams!', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
