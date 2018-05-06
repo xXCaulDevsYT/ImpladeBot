f9ec61ea20ac78eb5e6abf39cfb484ed81d30045
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-06 10:07:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setActivity('Malayman is online!', { type: 'WATCHING' });
 client.user.setStatus("dnd");
+client.user.setActivity("Malayman is online!");
 });
 
 client.on('message', message => {
