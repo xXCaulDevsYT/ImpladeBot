881ab29fa6d3de3dc5a14dc15a6ac7516add4b1c
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-06 17:06:06 +0800

    Re-made!

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// Discord.js bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'ZythronPE on Minecraft!'});
+    client.user.setActivity('https://github.com/ImpladeDeveloped/ImpladeBot', {type: 'ZythronPE'});
 });
 
 client.on('message', message => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !message.guild) return;
+    if (!message.content.startsWith(process.env.PREFIX) || !message.guild) return;
     const command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = message.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    if (command === 'guide') return message.channel.send('https://github.com/ImpladeDeveloped/ImpladeBot');
+    else if (command === 'invite') return message.channel.send(process.env.INVITE);
     if (message.content === 'Hi') {
     	message.reply('Heyo there!');
   	}
