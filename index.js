3cc6a5f55cf1140c35f2a42c5ce1353bb2f5073c
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-06 17:29:59 +0800

    Re-made?!

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://github.com/ImpladeDeveloped/ImpladeBot', {type: 'ZythronPE'});
+	console.log('I am ready!');
+    client.user.setActivity('ZythronPE');
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(process.env.PREFIX) || !message.guild) return;
-    const command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = message.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return message.channel.send('https://github.com/ImpladeDeveloped/ImpladeBot');
-    else if (command === 'invite') return message.channel.send(process.env.INVITE);
+    if (message.content === 'Hi') {
+    	message.reply('Heyo there!');
+  	}
 });
 
 client.login(process.env.TOKEN);
