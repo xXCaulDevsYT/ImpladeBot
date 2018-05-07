426e9e8f1e68e764bcfa181897e3e10254170e24
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-07 06:26:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('for school exams!', {type: 'WATCHING'});
+    client.user.setActivity('using a  code for GhostView plugin! | Type $help', {type: 'PLAYING'});
 });
 
 client.on('message', msg => {
-    if (msg.content === 'Hi') {
-    	msg.reply('Heyo, buddy!');
+    if (msg.content === '$help') {
+    	msg.reply('**__Commands List__** ```[ $server | $info | $ghostview | $help ]``` **__Updated By__** Zadezter#0207');
   	}
         if (msg.content === 'Zade') {
-    	msg.reply('What do you want talking about to him?');
+    	msg.reply('You have called him. Please tell, what do you want to talk about it?');
   	}
-        if (msg.content === 'Bye') {
-    	msg.reply('Okay, bye!');
+        if (msg.content === '$ghostview') {
+    	msg.reply('**Download the latest plugin builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what it made by Zadezter at:** __http://github.com/Zadezter/GhostView__');
   	}
-        if (msg.content === 'Server') {
-    	msg.reply('**This is ZythronPE server!** ```IP: play.zythronpe.ml | Port: 19132```');
+        if (msg.content === '$server') {
+    	msg.reply('**This is a ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
   	}
-        if (msg.content === 'nub') {
-    	msg.reply('Shut up! You are too noob actually!');
-  	}
-        if (msg.content === 'noob') {
-    	msg.reply('Shut up! You are too noob actually!');
-  	}
-        if (msg.content === 'DM') {
-    	msg.reply('Okay? Go DMs!');
-  	}
-        if (msg.content === 'Noob') {
-    	msg.reply('Shut up! You are too noob actually!');
-  	}
-        if (msg.content === 'Nub') {
-    	msg.reply('Shut up! You are too noob actually!');
+        if (msg.content === '$info) {
+    	msg.reply('**This bot was created on 6 May 2018 by using a Discord.js/Node.js for this function code!** ```---``` **Version**: IMB-12 ```---``` **Owned by** Zadezter#0207');
   	}
 });
 
