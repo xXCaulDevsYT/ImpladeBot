fbb99beb86dfac0970aeeec777cc1c135dfd23b2
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-07 15:11:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ client.on('ready', () => {
     client.user.setActivity('ZythronPE on Minecraft!', {type: 'PLAYING'});
 });
 
-client.on('message', msg => {
-    if (command === '$help') {
+client.on('message', async msg => {
+    if(message.author.bot) return;
+    
+    if (msg.content === '$help') {
     	msg.channel.send('**__Commands List__** ```[ $server | $info | $ghostview | $help ]``` **__Updated By__** Zadezter#0207');
   	} else
-        if (command === 'Zade') {
+        if (msg.content === 'Zade') {
     	msg.channel.send('You have called him. Please tell, what do you want to talk about it?');
   	} else
-        if (command === '$ghostview') {
+        if (msg.content === '$ghostview') {
     	msg.channel.send('**Download the latest plugin builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what it made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
   	} else
-        if (command === '$server') {
+        if (msg.content === '$server') {
     	msg.channel.send('**This is a ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
   	} else
-        if (command === '$info') {
+        if (msg.content === '$info') {
     	msg.channel.send('**This bot was created on 6 May 2018! Using a code with Discord.js!** ```---``` **Version**: IMB-1.0 (BETA) ```---``` **It is owned by** Zadezter#0207');
   	}
 });
