e39920fc966dfc44c890f10a8920af1c37c6a783
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-07 06:40:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ client.on('message', msg => {
     	msg.reply('You have called him. Please tell, what do you want to talk about it?');
   	}
         if (msg.content === '$ghostview') {
-    	msg.reply('**Download the latest plugin builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what it made by Zadezter at:** __http://github.com/Zadezter/GhostView__');
+    	msg.reply('**Download the latest plugin builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what it made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
   	}
         if (msg.content === '$server') {
     	msg.reply('**This is a ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
   	}
         if (msg.content === '$info') {
-    	msg.reply('**This bot was created on 6 May 2018 by using a Discord.js/Node.js for this function code!** ```---``` **Version**: IMB-12 ```---``` **Owned by** Zadezter#0207');
+    	msg.reply('**This bot was created on 6 May 2018! Using a code with Discord.js!** ```---``` **Version**: IMB-1.0 (BETA) ```---``` **It is owned by** Zadezter#0207');
   	}
 });
 
