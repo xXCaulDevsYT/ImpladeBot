038bdf3bf4d77aa49053da066132909c6f1f88ca
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2018-05-10 23:20:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ client.on('message', async msg => {
   	} else
         if (msg.content === '$info') {
     	msg.channel.send('**This bot was created on 6 May 2018! Using a code with Discord.js!** ```---``` **Version**: IMB-1.0 (BETA) ```---``` **It is owned by** Zadezter#0207');
-  	}
+  	} else
+        if (msg.contsnt === '$github') {
+        msg.channel.send('__GITHUB TEAM__ https://github.com/ImpladeDeveloped
 });
 
 client.login(process.env.BOT_TOKEN);
