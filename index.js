e3b15a1307768bb22e85f2b94241aba87fb8eb7d
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-11 07:28:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('for 4 more days in holiday!', {type: 'WATCHING'});
+    client.user.setActivity('for games!', {type: 'STREAMING'});
 });
 
 client.on('message', async msg => {
@@ -19,13 +19,13 @@ client.on('message', async msg => {
     	msg.channel.send('**Download the latest plugins builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what is made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
   	} else
         if (msg.content === '$server') {
-    	msg.channel.send('**This is the ZythronPE server! Come and play if you have the time!** ```IP: play.zythronpe.ml | Port: 19132```');
+    	msg.channel.send('**This is the ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
   	} else
         if (msg.content === '$info') {
     	msg.channel.send('**This bot was created on 6 May 2018! Using code with Discord.js!** ```---``` **Version**: IMB-1.0 (BETA) ```---``` **It is owned by** Zadezter#0207');
   	} else
         if (msg.content === '$github') {
-        msg.channel.send('__GITHUB TEAM__: https://github.com/ImpladeDeveloped');
+        msg.channel.send('**Our Github Team For Coding**: https://github.com/ImpladeDeveloped');
     }
 });
 
