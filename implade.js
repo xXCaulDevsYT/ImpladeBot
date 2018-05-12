7587f711a49a1cca0750d6f9403a0d59553dfc61
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-12 07:42:09 +0000

    Update and rename index.js to implade.js

--- implade.js
+++ implade.js
@@ -10,13 +10,13 @@ client.on('message', async msg => {
     if(msg.author.bot) return;
     
     if (msg.content === '$help') {
-    	msg.channel.send('**__Commands List__** ```[ $server | $info | $ghostview | $help | $github ]```[] ```[ $version ]```');
+    	msg.channel.send('**__Commands List__** ```[ $server | $info | $ghostview | $help | $github ]``````[ $version ]```');
     } else
         if (msg.content === 'Zade') {
-    	msg.channel.send('You called @Zadezter#0207. Please tell, what do you want to talk about?');
+    	msg.channel.send('You called him. Please tell, what do you want to talk about?');
     } else
         if (msg.content === '$ghostview') {
-    	msg.channel.send('**Download the latest plugins builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__ ```---``` **Check all source code what is made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
+    	msg.channel.send('**Download the latest plugins builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__``` Poggit ¡ | Github ! ```**Check all source code what is made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
     } else
         if (msg.content === '$server') {
     	msg.channel.send('**This is the ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
@@ -25,7 +25,7 @@ client.on('message', async msg => {
     	msg.channel.send('**This bot was created on 6 May 2018 by using code with Discord.js!** [Owner: __Zadezter#0207__]');
     } else
         if (msg.content === '$version') {
-        msg.channel.send('**This bot is running a language code** __discord.js__ **on version IB-1.3 with using a Heroku Host 24/7!**');
+        msg.channel.send('**This bot is running a language code** __discord.js__ **on version IB-1.3 with using a Heroku Server Hosting 24/7!**');
     }
 });
 
